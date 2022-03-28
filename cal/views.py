1d04e7f8be10c2b1d0e1b72ab2d3d4f2d3c0e6a2
Raviteja <raviteja.adhoni88@gmail.com>
2022-03-28 17:00:05 +0530

    Frontend built with bg image

--- cal/views.py
+++ cal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Cal
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello world')
+	cal = Cal.objects.all()
+	# return HttpResponse('Hello world')
+	return render(request, 'index.html')
